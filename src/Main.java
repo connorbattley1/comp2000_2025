d6620b3a894d7d372ed2f71d6246301d4c455943
connorbattley1 <79952455+connorbattley1@users.noreply.github.com>
2025-08-11 14:04:24 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("~~~~~\n" +
-                        "COMP2000 2025\n" +
-                        "~~~~~");
+        System.out.println("""
+                           ~~~~~
+                           COMP2000 2025
+                           ~~~~~""");
     }
 }

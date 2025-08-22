dcdbec1e9a21e3467e24507b17910e44e14e4cd8
connorbattley1 <79952455+connorbattley1@users.noreply.github.com>
2025-08-22 12:03:01 +1000

    contains, rectangle, super implementation

--- src/Cell.java
+++ src/Cell.java
@@ -1,17 +1,15 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Cell {
+public class Cell extends Rectangle {
   // fields
-  int x;
-  int y;
   static int size = 35;
 
   // constructors
   public Cell(int inX, int inY) {
-    x = inX;
-    y = inY;
+    super(inX, inY, size, size);
   }
 
   // methods
@@ -21,14 +19,14 @@ public class Cell {
     } else {
       g.setColor(Color.WHITE);
     }
-    g.fillRect(x, y, size, size);
+    g.fillRect(x, y, width, height);
     g.setColor(Color.BLACK);
-    g.drawRect(x, y, size, size);
+    g.drawRect(x, y, width, height);
   }
 
   public boolean contains(Point p) {
     if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
+      return super.contains(p);
     } else {
       return false;
     }

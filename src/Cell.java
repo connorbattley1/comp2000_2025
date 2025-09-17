2ae049bec8bda4e731e5c129fdd6b291e1d97be8
connorbattley1 <79952455+connorbattley1@users.noreply.github.com>
2025-09-17 19:19:31 +1000

    Apply terain chages using cell class. After testing the terrain is now visible when the program is run and is spread using hard coded percentages

--- src/Cell.java
+++ src/Cell.java
@@ -2,34 +2,30 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
-//test
-public class Cell extends Rectangle {
-  static int size = 35;
-  char col;
-  int row;
 
-  public Cell(char inCol, int inRow, int x, int y) {
+public class Cell extends Rectangle {
+  public static int size = 35;
+  public final char col;
+  public final int row;
+  protected Color base;
+
+  public Cell(char inCol, int inRow, int x, int y, Color color) {
     super(x, y, size, size);
-    col = inCol;
-    row = inRow;
+    this.col = inCol;
+    this.row = inRow;
+    this.base = color;
   }
 
-  public void paint(Graphics g, Point mousePos) {
-    if(contains(mousePos)) {
-      g.setColor(Color.GRAY);
-    } else {
-      g.setColor(Color.WHITE);
-    }
+  public void paint(Graphics g, Point mouse) {
+    // Always render the cell
+    g.setColor(base);
     g.fillRect(x, y, size, size);
     g.setColor(Color.BLACK);
     g.drawRect(x, y, size, size);
-  }
-
-  public boolean contains(Point p) {
-    if(p != null) {
-      return super.contains(p);
-    } else {
-      return false;
+    // Only darken if mouse is present and inside
+    if (mouse != null && contains(mouse)) {
+      g.setColor(new Color(0, 0, 0, 50)); // semi-transparent overlay
+      g.fillRect(x, y, size, size);
     }
   }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,40 +1,39 @@
-import java.awt.Graphics;
 import java.awt.Point;
 import java.util.Optional;
+import java.util.Random;
 
 public class Grid {
-  Cell[][] cells = new Cell[20][20];
-  
+  public Cell[][] cells = new Cell[20][20]; // make sure this is public or has an accessor
+
   public Grid() {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j] = new Cell(colToLabel(i), j, 10+Cell.size*i, 10+Cell.size*j);
+    Random rnd = new Random(42);
+    for (int col = 0; col < cells.length; col++) {
+      for (int row = 0; row < cells[col].length; row++) {
+        char label = colToLabel(col);
+        int x = 10 + Cell.size * col;
+        int y = 10 + Cell.size * row;
+        int r = rnd.nextInt(100);
+        // ~60% grass, 25% sand, 15% water
+        if (r < 60) {
+          cells[col][row] = new GrassCell(label, row, x, y);
+        } else if (r < 85) {
+          cells[col][row] = new SandCell(label, row, x, y);
+        } else {
+          cells[col][row] = new WaterCell(label, row, x, y);
+        }
       }
     }
   }
 
-  private char colToLabel(int col) {
-    return (char) (col + Character.valueOf('A'));
-  }
+  // Helper:
+  private char colToLabel(int c) { return (char)('A' + c); }
 
-  private int labelToCol(char col) {
-    return (int) (col - Character.valueOf('A'));
-  }
-
-  public void paint(Graphics g, Point mousePos) {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
-      }
-    }
-  }
+  private int labelToCol(char c) { return c - 'A'; }
 
   public Optional<Cell> cellAtColRow(int c, int r) {
-    if(c >= 0 && c < cells.length && r >=0 && r < cells[c].length) {
+    if (c >= 0 && c < cells.length && r >= 0 && r < cells[c].length)
       return Optional.of(cells[c][r]);
-    } else {
-      return Optional.empty();
-    }
+    return Optional.empty();
   }
 
   public Optional<Cell> cellAtColRow(char c, int r) {
@@ -42,13 +41,22 @@ public class Grid {
   }
 
   public Optional<Cell> cellAtPoint(Point p) {
-    for(int i=0; i < cells.length; i++) {
-      for(int j=0; j < cells[i].length; j++) {
-        if(cells[i][j].contains(p)) {
-          return Optional.of(cells[i][j]);
+    if (p == null) return Optional.empty();
+      for (Cell[] cell : cells) {
+        for (Cell cell1 : cell) {
+            if (cell1.contains(p)) {
+                return Optional.of(cell1);
+            }
         }
       }
-    }
     return Optional.empty();
+  }
+
+  public void paint(java.awt.Graphics g, Point mouse) {
+      for (Cell[] cell : cells) {
+          for (Cell cell1 : cell) {
+              cell1.paint(g, mouse);
+          }
+      }
   }
 }
